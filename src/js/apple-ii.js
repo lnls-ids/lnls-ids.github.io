76df64c03c7da51bbe7fda231b342e9d773dc978
Jefferson Barros Vieira <jefferson.negociom03@gmail.com>
2025-08-15 10:13:53 -0300

    added movimentation functions

--- src/js/apple-ii.js
+++ src/js/apple-ii.js
@@ -5,12 +5,12 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 // camera.rotation.x += Math.PI/2;
 camera.lookAt(0, 0, 0);
-camera.position.set(30, 10, 100);
+camera.position.set(-160, 70, 100);
 camera.updateProjectionMatrix();
 
 // RENDER CREATE
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth, window.innerHeight - 100);
 renderer.setClearColor(0xb1c4d8, 1);
 document.body.appendChild(renderer.domElement);
 
@@ -26,18 +26,21 @@ const eletron_radius = 1;
 
 // Create a sphere to represent the particle
 const particleGeometry = new THREE.SphereGeometry(eletron_radius, 35, 35);
-const particleMaterial = new THREE.MeshStandardMaterial({ color: 0xff0000 });
+const particleMaterial = new THREE.MeshStandardMaterial({ color: 0x2e6f40 });
 const particle = new THREE.Mesh(particleGeometry, particleMaterial);
 scene.add(particle);
 
 // Update block positions based on gap
 const gap = document.getElementById('gap');
 const period = document.getElementById('period');
+const rho = document.getElementById('rho')
+const phi = document.getElementById('phi')
 
 const block_width = 30;
 const block_height = 20;
 const d_between = 1;
-const und_period_number = 10;
+const d_casset = 1;
+const und_period_number = 5;
 const und_period = period.value;
 let block_length = period.value / 4 - d_between;
 
@@ -56,53 +59,187 @@ const blockMaterialRed = new THREE.MeshStandardMaterial({
     opacity: 0.6
 });
 
-// Casset sup
-let casset_sup = [];
-let casset_sup_contour = [];
-for(let i = 0; i < 4*und_period_number; i++){
-    const block = new THREE.Mesh(blockGeometry, i%2 == 0 ? blockMaterialBlue : blockMaterialRed);
-    block.position.set(i*(block_length+d_between) - und_period*und_period_number/2 + (d_between+block_length)/2, (block_height + und_gap) / 2, 0);
-    casset_sup.push(block);
+let mode = 1;
+
+window.alternateMode = (mode_type) => {
+    if (mode_type == "in-phase") {
+        document.getElementById('in-phase').checked = true;
+        document.getElementById('counter-phase').checked = false;
+        mode = 1;
+    }
+    if (mode_type == "counter-phase") {
+        document.getElementById('counter-phase').checked = true;
+        document.getElementById('in-phase').checked = false;
+        mode = -1;
+    }
+}
+
+
+// Casset Up Left
+let casset_up_left = [];
+let casset_up_left_contour = [];
+let casset_up_left_pos0 = []
+for (let i = 0; i < 4 * und_period_number; i++) {
+    const block = new THREE.Mesh(blockGeometry, i % 2 == 0 ? blockMaterialBlue : blockMaterialRed);
+    block.position.set(i * (block_length + d_between) - und_period * und_period_number / 2 + (d_between + block_length) / 2, (block_height + und_gap) / 2, -(block_width + d_casset) / 2);
+    casset_up_left.push(block);
+    casset_up_left_pos0.push([i * (block_length + d_between) - und_period * und_period_number / 2 + (d_between + block_length) / 2, (block_height + und_gap) / 2, -(block_width + d_casset) / 2])
     scene.add(block);
 
     const blockEdges1 = new THREE.EdgesGeometry(blockGeometry);
     const blockLine1 = new THREE.LineSegments(blockEdges1, new THREE.LineBasicMaterial({ color: 0x00000f }));
     blockLine1.position.copy(block.position);
-    casset_sup_contour.push(blockLine1);
+    casset_up_left_contour.push(blockLine1);
     scene.add(blockLine1);
 }
 
-// Casset inf
-let casset_inf = [];
-let casset_inf_contour = [];
-for(let i = 0; i < 4*und_period_number; i++){
-    const block = new THREE.Mesh(blockGeometry, i%2 == 0 ? blockMaterialRed : blockMaterialBlue);
-    block.position.set(i*(block_length+d_between) - und_period*und_period_number/2 + (d_between+block_length)/2, -(block_height + und_gap) / 2, 0);
-    casset_inf.push(block);
+// Casset Up Right
+let casset_up_right = [];
+let casset_up_right_contour = [];
+let casset_up_right_pos0 = [];
+for (let i = 0; i < 4 * und_period_number; i++) {
+    const block = new THREE.Mesh(blockGeometry, i % 2 == 0 ? blockMaterialBlue : blockMaterialRed);
+    block.position.set(i * (block_length + d_between) - und_period * und_period_number / 2 + (d_between + block_length) / 2, (block_height + und_gap) / 2, (block_width + d_casset) / 2);
+    casset_up_right.push(block);
+    casset_up_right_pos0.push([i * (block_length + d_between) - und_period * und_period_number / 2 + (d_between + block_length) / 2, (block_height + und_gap) / 2, (block_width + d_casset) / 2])
     scene.add(block);
 
     const blockEdges1 = new THREE.EdgesGeometry(blockGeometry);
     const blockLine1 = new THREE.LineSegments(blockEdges1, new THREE.LineBasicMaterial({ color: 0x00000f }));
     blockLine1.position.copy(block.position);
-    casset_inf_contour.push(blockLine1)
+    casset_up_right_contour.push(blockLine1);
     scene.add(blockLine1);
 }
+
+// Casset Bottom Left
+let casset_bottom_left = [];
+let casset_bottom_left_contour = [];
+let casset_bottom_left_pos0 = [];
+for (let i = 0; i < 4 * und_period_number; i++) {
+    const block = new THREE.Mesh(blockGeometry, i % 2 == 0 ? blockMaterialRed : blockMaterialBlue);
+    block.position.set(i * (block_length + d_between) - und_period * und_period_number / 2 + (d_between + block_length) / 2, -(block_height + und_gap) / 2, -(block_width + d_casset) / 2);
+    casset_bottom_left.push(block);
+    casset_bottom_left_pos0.push([i * (block_length + d_between) - und_period * und_period_number / 2 + (d_between + block_length) / 2, -(block_height + und_gap) / 2, -(block_width + d_casset) / 2])
+    scene.add(block);
+
+    const blockEdges1 = new THREE.EdgesGeometry(blockGeometry);
+    const blockLine1 = new THREE.LineSegments(blockEdges1, new THREE.LineBasicMaterial({ color: 0x00000f }));
+    blockLine1.position.copy(block.position);
+    casset_bottom_left_contour.push(blockLine1)
+    scene.add(blockLine1);
+}
+
+// Casset Bottom Right
+let casset_bottom_right = [];
+let casset_bottom_right_contour = [];
+let casset_bottom_right_pos0 = [];
+for (let i = 0; i < 4 * und_period_number; i++) {
+    const block = new THREE.Mesh(blockGeometry, i % 2 == 0 ? blockMaterialRed : blockMaterialBlue);
+    block.position.set(i * (block_length + d_between) - und_period * und_period_number / 2 + (d_between + block_length) / 2, -(block_height + und_gap) / 2, (block_width + d_casset) / 2);
+    casset_bottom_right.push(block);
+    casset_bottom_right_pos0.push([i * (block_length + d_between) - und_period * und_period_number / 2 + (d_between + block_length) / 2, -(block_height + und_gap) / 2, (block_width + d_casset) / 2])
+    scene.add(block);
+
+    const blockEdges1 = new THREE.EdgesGeometry(blockGeometry);
+    const blockLine1 = new THREE.LineSegments(blockEdges1, new THREE.LineBasicMaterial({ color: 0x00000f }));
+    blockLine1.position.copy(block.position);
+    casset_bottom_right_contour.push(blockLine1)
+    scene.add(blockLine1);
+}
+
+// Period control
+period.addEventListener('input', () => {
+    let new_block_length = period.value / 4 - d_between
+    let rho_value = parseFloat(rho.value)
+    let phi_value = parseFloat(phi.value)
+
+    let value;
+
+    casset_up_left.forEach((block, idx) => {
+        value = idx * (new_block_length + d_between + period.value / 4) / 2 - period.value * und_period_number / 2 + (d_between + new_block_length) / 2
+        block.position.x = value - mode * rho_value;
+        block.scale.x = new_block_length / block_length;
+        casset_up_left_pos0[idx][0] = value
+    })
+    casset_up_left_contour.forEach((contour, idx) => {
+        value = idx * (new_block_length + d_between + period.value / 4) / 2 - period.value * und_period_number / 2 + (d_between + new_block_length) / 2
+        contour.position.x = value - mode * rho_value;
+        contour.scale.x = new_block_length / block_length
+    })
+
+    casset_up_right.forEach((block, idx) => {
+        value = idx * (new_block_length + d_between + period.value / 4) / 2 - period.value * und_period_number / 2 + (d_between + new_block_length) / 2
+        block.position.x = value - mode * (rho_value + phi_value);
+        block.scale.x = new_block_length / block_length
+        casset_up_right_pos0[idx][0] = value
+    })
+    casset_up_right_contour.forEach((contour, idx) => {
+        value = idx * (new_block_length + d_between + period.value / 4) / 2 - period.value * und_period_number / 2 + (d_between + new_block_length) / 2
+        contour.position.x = value - mode * (rho_value + phi_value);
+        contour.scale.x = new_block_length / block_length
+    })
+
+    casset_bottom_left.forEach((block, idx) => {
+        value = idx * (new_block_length + d_between + period.value / 4) / 2 - period.value * und_period_number / 2 + (d_between + new_block_length) / 2
+        block.position.x = value - phi_value;
+        block.scale.x = new_block_length / block_length
+        casset_bottom_left_pos0[idx][0] = value
+    })
+    casset_bottom_left_contour.forEach((contour, idx) => {
+        value = idx * (new_block_length + d_between + period.value / 4) / 2 - period.value * und_period_number / 2 + (d_between + new_block_length) / 2
+        contour.position.x = value - phi_value;
+        contour.scale.x = new_block_length / block_length
+    })
+
+    casset_bottom_right.forEach((block, idx) => {
+        value = idx * (new_block_length + d_between + period.value / 4) / 2 - period.value * und_period_number / 2 + (d_between + new_block_length) / 2
+        block.position.x = value;
+        block.scale.x = new_block_length / block_length
+        casset_bottom_right_pos0[idx][0] = value
+    })
+    casset_bottom_right_contour.forEach((contour, idx) => {
+        value = idx * (new_block_length + d_between + period.value / 4) / 2 - period.value * und_period_number / 2 + (d_between + new_block_length) / 2
+        contour.position.x = value;
+        contour.scale.x = new_block_length / block_length
+    })
+
+    particle.position.x = - period.value * und_period_number / 2;
+    particle.position.z = 0;
+    trailPoints = [];
+    z_mean = 0;
+    t = 0;
+});
 
 // Gap control
 gap.addEventListener('input', () => {
     const distance = parseFloat(gap.value);
 
-    casset_sup.forEach( block => {
+    casset_up_left.forEach(block => {
         block.position.y = (block_height + distance) / 2;
     })
-    casset_sup_contour.forEach( block => {
+    casset_up_left_contour.forEach(block => {
         block.position.y = (block_height + distance) / 2;
     })
 
-    casset_inf.forEach( block => {
+    casset_up_right.forEach(block => {
+        block.position.y = (block_height + distance) / 2;
+    })
+    casset_up_right_contour.forEach(block => {
+        block.position.y = (block_height + distance) / 2;
+    })
+
+
+    casset_bottom_left.forEach(block => {
         block.position.y = -(block_height + distance) / 2;
     })
-    casset_inf_contour.forEach( block => {
+    casset_bottom_left_contour.forEach(block => {
+        block.position.y = -(block_height + distance) / 2;
+    })
+
+    casset_bottom_right.forEach(block => {
+        block.position.y = -(block_height + distance) / 2;
+    })
+    casset_bottom_right_contour.forEach(block => {
         block.position.y = -(block_height + distance) / 2;
     })
 
@@ -114,51 +251,47 @@ gap.addEventListener('input', () => {
 
 });
 
-const casset_sup0 = casset_sup;
-const casset_sup_contour0 = casset_sup_contour;
+// Rho control
+rho.addEventListener('input', () => {
+    let rho_value = parseFloat(rho.value)
+    let phi_value = parseFloat(phi.value)
 
-// Period control
-period.addEventListener('input', () => {
-    let new_block_length = period.value / 4 - d_between
-
-    casset_sup.forEach( (block, idx) => {
-        block.position.x = idx*(new_block_length+d_between + period.value/4)/2 - period.value*und_period_number/2 + (d_between+new_block_length)/2;
-        block.scale.x = new_block_length/block_length
+    casset_up_left.forEach((block, idx) => {
+        block.position.x = casset_up_left_pos0[idx][0] - mode * rho_value;
     })
-    casset_sup_contour.forEach( (contour, idx) => {
-        contour.position.x = idx*(new_block_length+d_between + period.value/4)/2 - period.value*und_period_number/2 + (d_between+new_block_length)/2;
-        contour.scale.x = new_block_length/block_length
+    casset_up_left_contour.forEach((block, idx) => {
+        block.position.x = casset_up_left_pos0[idx][0] - mode * rho_value;
     })
 
-    casset_inf.forEach( (block, idx) => {
-        block.position.x = idx*(new_block_length+d_between + period.value/4)/2 - period.value*und_period_number/2 + (d_between+new_block_length)/2;
-        block.scale.x = new_block_length/block_length
+    casset_up_right.forEach((block, idx) => {
+        block.position.x = casset_up_right_pos0[idx][0] - mode *(rho_value + phi_value);
     })
-    casset_inf_contour.forEach( (contour, idx) => {
-        contour.position.x = idx*(new_block_length+d_between + period.value/4)/2 - period.value*und_period_number/2 + (d_between+new_block_length)/2;
-        contour.scale.x = new_block_length/block_length
+    casset_up_right_contour.forEach((block, idx) => {
+        block.position.x = casset_up_right_pos0[idx][0] -mode * (rho_value + phi_value);
     })
 
-    particle.position.x = - period.value * und_period_number / 2;
-    particle.position.z = 0;
-    trailPoints = [];
-    z_mean = 0;
-    t = 0;
 });
 
+// Phi control
+phi.addEventListener('input', () => {
+    let phi_value = parseFloat(phi.value)
+    let rho_value = parseFloat(rho.value)
 
-// Add axes helper for reference
-// const axesHelper = new THREE.AxesHelper(20);
-//reset axes colors
-// var colors = axesHelper.geometry.attributes.color;
+    casset_up_right.forEach((block, idx) => {
+        block.position.x = casset_up_right_pos0[idx][0] - mode * (phi_value + rho_value);
+    })
+    casset_up_right_contour.forEach((block, idx) => {
+        block.position.x = casset_up_right_pos0[idx][0] - mode * (phi_value + rho_value);
+    })
 
-// colors.setXYZ( 0, 1, 1, 0 ); // index, R, G, B
-// colors.setXYZ( 1, 1, 0, 0 ); // red
-// colors.setXYZ( 2, 1, 1, 0 );
-// colors.setXYZ( 3, 0, 1, 0 ); // green
-// colors.setXYZ( 4, 1, 0, 1 );
-// colors.setXYZ( 5, 1, 1, 1 ); // blue
-// scene.add(axesHelper);
+    casset_bottom_left.forEach((block, idx) => {
+        block.position.x = casset_bottom_left_pos0[idx][0] - phi_value;
+    })
+    casset_bottom_left_contour.forEach((block, idx) => {
+        block.position.x = casset_bottom_left_pos0[idx][0] - phi_value;
+    })
+
+});
 
 // Add orbit controls for mouse interaction
 const controls = new THREE.OrbitControls(camera, renderer.domElement);
@@ -182,7 +315,7 @@ let halbach = {
 }
 
 let Br = 1.3
-let B = Br*halbach['a']*Math.exp(halbach['b']*(gap.value/period.value) + halbach['c']*(gap.value/period.value)**2)
+let B = Br * halbach['a'] * Math.exp(halbach['b'] * (gap.value / period.value) + halbach['c'] * (gap.value / period.value) ** 2)
 let K = 93.36 * B * period.value * 1e-3;
 let gamma = 5;
 
@@ -191,7 +324,7 @@ let mean_beta_z = 1 - (1 / (2 * gamma ** 2)) - (K ** 2 / (4 * gamma ** 2 * beta_
 let z_mean = 0;
 let beta_z = 0;
 
-particle.position.x =  - period.value * und_period_number /2
+particle.position.x = - period.value * und_period_number / 2
 particle.position.y = 0
 particle.position.z = 0
 
@@ -212,35 +345,6 @@ scene.add(trailLine);
 // Animation function
 function animate() {
     requestAnimationFrame(animate);
-
-    // Move the particle back and forth between the blocks
-    z_mean += mean_beta_z*dt
-    B = Br*halbach['a']*Math.exp(halbach['b']*(gap.value/period.value) + halbach['c']*(gap.value/period.value)**2)
-    K = 93.36 * B * period.value * 1e-3
-
-    beta_x = (K / gamma) * Math.cos((2 * Math.PI / (period.value)) * z_mean)
-    beta_z = mean_beta_z - (K**2/(4*gamma**2 * beta_s))*Math.cos((4*Math.PI/(period.value))*z_mean)
-
-    particle.position.x += beta_z * dt ;
-    particle.position.z +=  beta_x * dt;
-
-    // Update trail
-    trailPoints.push(particle.position.clone());
-    if (trailPoints.length > trailLength) {
-        trailPoints.shift();
-    }
-    trailGeometry.setFromPoints(trailPoints);
-
-    // Increment time
-    t += dt;
-
-    // Render the scene
-    if (particle.position.x > period.value * und_period_number / 2) {
-        particle.position.x = - period.value * und_period_number / 2;
-        particle.position.z = 0;
-        z_mean = 0;
-        trailPoints = [];
-    }
     renderer.render(scene, camera);
 }
 
